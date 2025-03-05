85f7af6ec0e8ee3d06a3bd9533c2dfc96ce5dd3b
Ntwali Chance Filme <ntwalichancefilme@gmail.com>
2025-03-05 08:35:07 +0200

    commit 05.03.2025

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -125,8 +125,8 @@ class CourseController extends Controller
                 'faculty_id'=>$request->faculty_id,
                 'label'=>$request->label
             ]);
-            
-            dd($request->all());
+
+            // dd($request->all());
             DB::commit();
             return back()->with(['success'=>'Curso actualizado com sucesso!']);
         } catch (Throwable $th) {
